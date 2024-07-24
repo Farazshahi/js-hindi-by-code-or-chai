e51cccdb072348e3f9fee6f0bfde519ad275c7e9
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-07-24 11:39:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "farazshahid20002@gmail.com"
+var accountPassword = "12345" //we do not prefer var cuz of issue block of scope
+accountCity = "jaipur"
+console.log(accountId); 
+// accountId = 2 not allowed
+accountEmail = "hitshh@gmail.com"
+accountCity = "delhi"
+var accountPassword = "121245"
+console.log(accountId,accountEmail,accountCity,accountPassword);
+//code readiblity is important
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+//type of datatypes
+/*number-=>123 range 2 to the power 53
+bigint= use for larger no.and use in bigger website and it use very less
+string => "" also stored in ''
+boolean => true or false
+null => stand alone value
+undefined=> when vslues isnt defined*/
