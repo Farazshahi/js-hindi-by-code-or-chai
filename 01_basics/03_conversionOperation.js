2aae65a1d07f9c8686ace2dd16b36afff1049e3a
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-07-25 15:02:01 +0000

    conversioan

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,6 +7,21 @@
 /*"33"=>33
 "33abc"=>NaN (NOT A NUMBER)
 true => 1 ; false => 0 */
-let isLoggedIn = 1
-let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanIsLoggedIn);
+// let isLoggedIn = 1
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+let str1 = "hello"
+let str2 = " faraz"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2+2);
+console.log(1+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+let gameCounter = 100 
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+//increament and decreament mdn se pdhna h
