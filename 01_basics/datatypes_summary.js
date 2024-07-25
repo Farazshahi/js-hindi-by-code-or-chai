745f10320518f62e4cdf12a5ed1e107a2420a83b
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-07-25 16:01:31 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+ /*There are two types of datatypes
+   1. primitive
+   2.Refrence(non primitive)
+
+   
+#Primitive
+1.string
+2.number
+3.boolean
+4.null
+5.undefined
+6.symbol
+7.BigInt
+
+#Refrence(Non Primitive)
+1.Array
+2.objects
+3.function
