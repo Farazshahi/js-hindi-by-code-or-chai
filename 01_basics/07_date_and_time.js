716ff463241b01ef42c489076f8e668156bb9aee
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-07-26 21:41:22 +0000

    date and time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toDateString())
+console.log(typeof myDate)
+
+let createMydate = new Date(2023,27,07)
+console.log(createMydate.toLocaleString())
