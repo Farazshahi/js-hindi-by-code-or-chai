60f9b32c91bcdc9d412358fa5e3b2171de2e8ced
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-07-28 13:32:38 +0000

    01_objects01

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,26 @@
+// singleton
+// Object.create
+
+// object litrals
+const mySym1 = Symbol("key1")
+
+const jsUser = {
+    name:"faraz",
+    [mySym1]:"myKey1",
+    "full name": "Faraz Shahid",
+    email:"farazshahid2002@gmail.com",
+    add:"tori amma ka ghr",
+    isLoggedIn: false
+}
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+// console.log(jsUser.full name);// if key are written in commas you cnt accs the value through dot(.)
+console.log(jsUser[mySym1]);
+console.log(typeof jsUser[mySym1])
+console.log(jsUser)
+jsUser.greetingtwo = function (){
+    console.log(`hello jsuser, ${this.name}`);
+}
+console.log(jsUser.greetingtwo());
+Object.freeze(jsUser);
