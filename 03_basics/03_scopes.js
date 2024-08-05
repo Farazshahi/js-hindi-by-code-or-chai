69e67a66fc969a58fae5efb452a0d63bd0912f3f
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-05 20:35:57 +0000

    scopes

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,11 +1,53 @@
+// let a = 300
+// if (true){
+//       let a = 5
+//       const b = 55
+//       var c = 60
+//       console.log("inner a",a);
 
-if (true){
-      let a = 5
-      const b = 55
-      var c = 60
 
-  }
+//   }
 //   console.log(a);
-//   console.log(b);
-  console.log(c);
+//   console.log(b);  
+//   console.log(c);
+
+
+// function one() {
+
+//   const username = "faraz"
+
+//   function two() {
+
+//     const website = "youtube"
+//     console.log(username);
+
+//   }
+//   two()
+
+// }
+// one()
+
+// if (true){
+//   const username = "faraz"
+//   if (username === "faraz"){
+//     const website = "youtube"
+//     console.log(username + website);
+    
+//   }
+//   console.log(website);
   
+// }
+// console.log(username);
+
+// +++++++++++++++++++++++++++++ intresting +++++++++++++++
+
+console.log(addone(5));
+
+function addone (num){
+  return num + 1
+}
+console.log(addtwo(5));
+
+const addtwo = function addtwo (num){
+  return num + 1
+}
