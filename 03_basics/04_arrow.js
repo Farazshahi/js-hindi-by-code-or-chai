84276f543b31dc304077b8ceccc6ee9dd8e9d7af
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-06 20:58:10 +0000

    arrow

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,48 @@
+// const user = {
+//     username: "faraz",
+//     price : 999,
+//      welcomeMessage : function (){
+//         console.log(`${this.username} ,welcime to website`);
+//         console.log(this);
+        
+//      }
+// }
+
+// user.welcomeMessage()
+// user.username = "harry"
+// user.welcomeMessage()
+// console.log(this);
+
+
+// function chai (){
+//     let username = "fataz"
+//     console.log(this.username)
+// }
+//  chai()
+
+// const chai = function (){
+//     let username = "faraz"
+//     console.log(this.username);
+
+// }
+//  chai()
+
+//  const chai = ()=>{
+//     let username = "faraz"
+//     console.log(this.username);
+    
+//  }
+//  chai()
+
+// const addTwo = (num1,num2)=>{
+
+//     return num1+num2
+// }
+// console.log(addTwo(7,5))
+
+// const addTwo = (num1,num2)=> num1+num2
+// const addTwo = (num1,num2)=> (num1+num2)
+const addTwo = (num1,num2)=> ({username :"faraz"})
+    console.log(addTwo(7,5))
+
+    
