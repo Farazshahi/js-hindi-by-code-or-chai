db4ac3c1363faee9df3a4a59a2e33f4a26d56240
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-11 12:36:09 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+// const userLoggedIn = true
+// if(userLoggedIn){
+//     console.log("true");
+    
+// }
+
+// <, > , <= , >= , ==, !=, ===(eqaul and check the type of data also)
+// const money = 1000
+// if (money < 500){
+//     console.log("true");
+    
+// }
+// else if (money > 500){
+//     console.log("true a");
+    
+// }
+// else if (money < 900){
+//     console.log("true b");
+    
+// }
+// else (money <1200)
+//     console.log("true c");
+// const balance = 1000 
+ 
+// if (balance >500) console.log("true a"),
+// console.log("true b"); // not a good method
+
+
+const userLoggedIn = true 
+const debitCard = true
+
+if( userLoggedIn && debitCard && 2 ==2){
+    console.log( true ,"allow to biy courses");
+    
+}
+const userLoggedInFromGoogle = true
+const userLoggedInFromGmail = true
+
+if ( userLoggedInFromGoogle || userLoggedInFromGmail ) {
+
+    console.log("allow to access");
+    
+}
