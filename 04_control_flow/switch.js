2875034888764f59a6a4856f14cc729e59d30478
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-11 15:01:25 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+//  const month = 3
+//  switch (month) {
+//     case 1 :
+//         console.log("no no");
+        
+//         break;
+//     case 2 :
+//         console.log("no NANA no");
+        
+//         break;
+//     case 3 :
+//         console.log("YES");
+        
+//         break;
+ 
+//     default:
+//         break;
+//  }
+ const month = "jan"
+ switch (month) {
+    case "jan" :
+        console.log("no no");
+        
+        break;
+    case "feb" :
+        console.log("no NANA no");
+        
+        break;
+    case "march" :
+        console.log("YES");
+        
+        break;
+ 
+    default:
+        break;
+ }
