6893c1783bbc293551edf7a78c346c30b95face5
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-11 19:21:04 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,50 @@
+ const userEmail = ""
+//  const mail = "farazshahi@mail.com"
+//  const mail = []
+
+// if (userEmail){
+//     console.log("truthy");
+    
+// }
+// else{
+//     console.log("falsy");
+    
+//    
+
+// ++++++++++++++++ falsy values
+
+// false , null , 0 , -0 , BigInt 0n ,NaN 
+
+// +++++++++++   truthy values
+
+// true , 1 , "0", 'false'  , " " space, [] empty array ,  function() empty function{}
+
+// if (userEmail.length === 0){
+//     console.log("EMPTY ARRAY");
+    
+// }
+ 
+// const emptyObject = {}
+//     if (Object.keys (emptyObject) .length === 0){
+//         console.log("epmty object");
+        
+//     }
+
+// Nullish coalescing operator (??) : null undefined
+
+let val1;
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+val1 = undefined ?? 151
+ 
+// console.log(val1);
+
+// condition ? true : false
+
+const icedTea = 100
+icedTea >= 80 ? console.log("less then 80"):console.log("more then 80");
+
+
+
