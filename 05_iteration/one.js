485fb019f293964951e14266d0fe7fd529a6ee12
Farazshahi <128748727+Farazshahi@users.noreply.github.com>
2024-08-12 21:01:53 +0000

    iteration

---
+++ 05_iteration/one.js
@@ -0,0 +1,58 @@
+// for (let index =0; index< array.length; index++){
+//     const element = array[index]
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//     const element = i; 
+//     if (i == 5){
+//         console.log(`detect 5`);
+        
+//     }
+//     console.log(element);
+    
+// }i
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`outer number of i ${i}`);
+
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`outer no of j ${j} and inner no ${i}`);
+//          console.log(i + "*" + j + "=" + i*j);
+         
+//     }
+    
+    
+// }
+
+// let myArray = ["flash","spiderman","batman"]
+// console.log(myArray.length);
+
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+// break and continue 
+
+// for (let index = 1; index <= 20; index++) {
+//     console.log(`value of i is ${index}`);
+//     if (index == 5) {
+//         console.log(`detect 5`);
+//         break
+        
+//     }
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    console.log(`value of i is ${index}`);
+    if (index == 5) {
+        console.log(`detect 5`);
+        continue
+        
+    }
+    
+}
---
+++ 05_iteration/three.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+    
+}
+const greeting = "hello world"
+
+for (const greet of greeting) {
+    
+    // console.log(greet);
+    
+    
+}
+
+const map = new Map()
+
+map.set("IND","India")
+map.set("SA","United State Of America")
+map.set("FR","France")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key,":-",value);
+    
+    
+}
+
+const myObj = {
+    game1 :"freefire",
+    gane2:"pubg"
+}
+
+for (const [key,value] of myObj) {
+    console.log(key,":-",value);
+    
+    
+}
+
